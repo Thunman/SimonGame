e3f169d91627ada61199f6aeb044999db520e918
Thunman <Thunman42@gmail.com>
2023-12-15 23:45:11 +0100

    adding consts for the diffrent elements

---
+++ main.ts
@@ -0,0 +1,6 @@
+const button = document.getElementById("startButton");
+const square1 = document.getElementById("square1");
+const square2 = document.getElementById("square2");
+const square3 = document.getElementById("square3");
+const square4 = document.getElementById("square4");
+
